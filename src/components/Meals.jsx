581648493e640a524ec3021bfff354ad6a81565a
dstrivedi <dstrivedi@users.noreply.github.com>
2023-04-22 14:45:52 +0000

    Meal component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,10 +1,29 @@
 
-import { useGlobalContext } from '../context'
+import { useGlobalContext } from '../context';
+import { BsHandThumbsUp } from "react-icons/bs";
 
 const Meals = () => {
-  const context = useGlobalContext()
-  console.log(context)
-  return <h1>Meal Component</h1>
+  const { meals } = useGlobalContext()
+  console.log(meals)
+  return (
+    <section className="section-center">
+      {
+        meals.map(singleMeal => {
+          const { idMeal, strMeal: title, strMealThumb: image } = singleMeal
+
+          return (
+            <article key={idMeal} className = "single-meal">
+              <img src={image} className="img"/>
+              <footer>
+                <h5>{title}</h5>
+                <button className="like-btn"><BsHandThumbsUp/></button>
+              </footer>
+            </article>
+          )
+        })
+      }
+    </section>
+  )
 }
 
 export default Meals
--- src/context.jsx
+++ src/context.jsx
@@ -1,21 +1,38 @@
-import React, { useContext , useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
+import axios from 'axios';
 
 const AppContext = React.createContext();
 
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
+
 const AppProvider = ({ children }) => {
 
+  const [meals,setMeals] = useState([])
+  
+  const fetchMeals = async (url) => {
+    try {
+      const {data} = await axios(url);
+      setMeals(data.meals)
+      // console.log(data);
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
   useEffect(() => {
-    const fetchData = async () => {
+    fetchMeals(allMealsUrl);
+    /*const fetchData = async () => {
       try {
         const response = await fetch('https://randomuser.me/api/');
         const data = await response.json();
         console.log(data)
-      } catch(error) {
+      } catch (error) {
         console.log(error)
       }
     }
 
-    fetchData()
+    fetchData() */
+
     /*fetch("https://randomuser.me/api/")
     .then((res) => {
       return res.json()
@@ -27,9 +44,9 @@ const AppProvider = ({ children }) => {
       console.log(err)
     }) */
   }, [])
-  
+
   return (
-    <AppContext.Provider value="hello">
+    <AppContext.Provider value={{ meals }}>
       {children}
     </AppContext.Provider>
   )
@@ -39,5 +56,5 @@ export const useGlobalContext = () => {
   return useContext(AppContext)
 }
 
-export {AppContext, AppProvider} 
+export { AppContext, AppProvider }
 
